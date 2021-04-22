49f445de369e99e5475f8a63195b4da16aef7245
Wilma <wilmapa@metropolia.fi>
2021-04-22 22:47:04 +0300

    Navigaatio lisätty, etusivua tyyllitelty, hakukenttä lisätty, slideshow lisätty etusivulle, lisää html sivuja

--- js/etusivu.js
+++ js/etusivu.js
@@ -1,1 +1,10 @@
 'use strict';
+let counter = 1;
+
+setInterval(function (){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+         counter = 1;
+    }
+}, 5000);
