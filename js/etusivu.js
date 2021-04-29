f6b02c3630e43413d87fe2897894d961fcf3404a
Wilma <wilmapa@metropolia.fi>
2021-04-29 10:07:53 +0300

    - Navigaatio korjattu - Etusivua tyyllitelty ja lisätty kaikkee hauskaa - Ostoskori sivu tehty - Kaikki tuotteet sivu tehty - Tuotetiedot sivu tehty (sinne ei vielä pääse minkään kautta).. - responsisuutta laitettu (koodi löytyy etusivu stylen pohjalta) - lisäsin "tili"-kohdan tuonne naviin, voi poistaa myöhemmin - kommentoin bannerit pois kun eivät menneet nätisti tuon navin kanssa, voidaan lisää myöhemmin - Paljon uusia placeholder kuvia/osa pysyviä varmaan

--- js/etusivu.js
+++ js/etusivu.js
@@ -1,10 +1,43 @@
 'use strict';
+
 let counter = 1;
 
 setInterval(function (){
     document.getElementById('radio' + counter).checked = true;
     counter++;
     if(counter > 4){
-         counter = 1;
+        counter = 1;
     }
 }, 5000);
+
+
+let MenuItems = document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight = "0px";
+function menutoggle(){
+    if(MenuItems.style.maxHeight === "0px"){
+        MenuItems.style.maxHeight = "200px"
+    }else{
+        MenuItems.style.maxHeight = "0px";
+    }
+
+}
+
+/* tuotteen minigalleria */
+
+let ProductImg = document.getElementById("ProductImg");
+let SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function (){
+    ProductImg.src = SmallImg[0].src;
+}
+SmallImg[1].onclick = function (){
+    ProductImg.src = SmallImg[1].src;
+}
+SmallImg[2].onclick = function (){
+    ProductImg.src = SmallImg[2].src;
+}
+SmallImg[3].onclick = function (){
+    ProductImg.src = SmallImg[3].src;
+}
+
